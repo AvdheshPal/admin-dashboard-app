84f216b3282bab26757f15442ef79d0f4adf9ea0
AvdheshPal <avdheshpal2201@gmail.com>
2024-11-23 14:44:39 +0530

    route changes

--- src/App.tsx
+++ src/App.tsx
@@ -4,18 +4,16 @@ import Login from "./pages/Login";
 import Profile from "./pages/Profile";
 import Home from "./pages/Home";
 import Layout from "./pages/Layout";
+import NoPage from "./pages/NoPage";
 
 const App: React.FC = () => {
     return (
         <Router>
             <Routes>
                 <Route path="/" element={<Layout />}>
-                    <Route index element={
-                            <Home />
-                    } />
-                    <Route path="/profile" element={
-                            <Profile />
-                    } />
+                    <Route index element={<Home />} />
+                    <Route path="/profile" element={<Profile />} />
+                    <Route path="*" element={<NoPage />} />
                 </Route>
                 <Route path="/login" element={<Login />} />
             </Routes>
