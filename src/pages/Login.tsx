f3da34bfe1384cebe57c418c373d617420612dd3
AvdheshPal <avdheshpal2201@gmail.com>
2024-11-25 07:51:28 +0530

    signup implemented

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -19,26 +19,30 @@ interface ForgotPasswordFormInputs {
 }
 
 interface AuthCardProps {
-    setForgetPassword: React.Dispatch<React.SetStateAction<boolean>>;
+    setWidget: React.Dispatch<React.SetStateAction<string>>;
+}
+
+interface SignupFormInputs {
+    name: string;
+    email: string;
+    password: string;
 }
 
 const Login: React.FC = () => {
-    const [isForgetPassword, setForgetPassword] = useState(false);
+    const [widget, setWidget] = useState('login');
 
     return (
         <div>
-            {isForgetPassword ? (
-                <ForgotPasswordCard setForgetPassword={setForgetPassword} />
-            ) : (
-                <LoginCard setForgetPassword={setForgetPassword} />
-            )}
+           {widget == 'login' && <LoginCard setWidget={setWidget} />}
+           {widget == 'signup' && <Signup setWidget={setWidget} />}
+           {widget == 'forgetPassword' && <ForgotPasswordCard setWidget={setWidget} />}
         </div>
     );
 };
 
 export default Login;
 
-const LoginCard: React.FC<AuthCardProps> = ({ setForgetPassword }) => {
+const LoginCard: React.FC<AuthCardProps> = ({ setWidget }) => {
     const reduxDispatch = useDispatch<AppDispatch>();
     const navigate = useNavigate();
 
@@ -178,6 +182,15 @@ const LoginCard: React.FC<AuthCardProps> = ({ setForgetPassword }) => {
                         )}
                     </div>
 
+                    <div className="mt-4">
+                    <p
+                        className="text-blue-600 hover:underline text-sm cursor-pointer"
+                        onClick={() => setWidget('signup')}
+                    >
+                       Create new account.
+                    </p>
+                </div>
+
                     {/* Submit Button */}
                     <Button
                         type="submit"
@@ -194,7 +207,7 @@ const LoginCard: React.FC<AuthCardProps> = ({ setForgetPassword }) => {
                 <div className="mt-4">
                     <p
                         className="text-blue-600 hover:underline text-sm cursor-pointer"
-                        onClick={() => setForgetPassword(true)}
+                        onClick={() => setWidget('forgetPassword')}
                     >
                         Forgot Password?
                     </p>
@@ -204,8 +217,187 @@ const LoginCard: React.FC<AuthCardProps> = ({ setForgetPassword }) => {
     );
 };
 
+const Signup: React.FC<AuthCardProps> = ({ setWidget }) => {
+    const [loading, setLoading] = useState(false);
+    const [showPassword, setShowPassword] = useState(false);
 
-const ForgotPasswordCard: React.FC<AuthCardProps> = ({ setForgetPassword }) => {
+    const { register, handleSubmit, formState: { errors } } = useForm<SignupFormInputs>();
+
+    const togglePasswordVisibility = () => {
+        setShowPassword((prev) => !prev);
+    };
+
+    const onSubmit: SubmitHandler<SignupFormInputs> = async (data) => {
+        setLoading(true);
+        toast.promise(
+            axios.post('https://x8ki-letl-twmt.n7.xano.io/api:IAuUhB5G/auth/signup', { ...data }),
+            {
+                loading: 'Signing Up...',
+                success: () => {
+                    setWidget('login');
+                    setLoading(false);
+                    return 'Signup Successful';
+                },
+                error: (error) => {
+                    setLoading(false);
+                    return `Error: ${error?.response?.data?.message || 'Failed to signup'}`;
+                },
+            }
+        );
+    };
+
+    return (
+        <div className="flex justify-center items-center min-h-screen">
+            <Card className="text-center">
+                <h2 className="text-2xl font-semibold text-gray-800 mb-4">Sign Up</h2>
+                <form onSubmit={handleSubmit(onSubmit)} className="space-y-4">
+                    {/* Name Field */}
+                    <div>
+                        <label htmlFor="name" className="block text-left text-gray-600">
+                            Name
+                        </label>
+                        <input
+                            type="text"
+                            id="name"
+                            {...register("name", { 
+                                required: "* Name is required",
+                                validate: (value) => value.trim() !== "" || "* Name cannot be empty or just whitespace"
+                             })}
+                            className={`w-full px-4 py-2 border rounded-md focus:outline-none ${errors.name ? "border-red-500" : "border-gray-300"}`}
+                            placeholder="Enter your name"
+                        />
+                        {errors?.name && (
+                            <p className="text-red-500 text-sm mt-1 text-left">
+                                {errors.name.message}
+                            </p>
+                        )}
+                    </div>
+
+                    {/* Email Field */}
+                    <div>
+                        <label htmlFor="email" className="block text-left text-gray-600">
+                            Email Address
+                        </label>
+                        <input
+                            type="email"
+                            id="email"
+                            {...register("email", {
+                                required: "* Email is required",
+                                pattern: {
+                                    value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,4}$/,
+                                    message: "* Enter a valid email address",
+                                },
+                            })}
+                            className={`w-full px-4 py-2 border rounded-md focus:outline-none ${errors.email ? "border-red-500" : "border-gray-300"}`}
+                            placeholder="Enter your email"
+                        />
+                        {errors?.email && (
+                            <p className="text-red-500 text-sm mt-1 text-left">
+                                {errors.email.message}
+                            </p>
+                        )}
+                    </div>
+
+                    {/* Password Field */}
+                    <div>
+                        <label htmlFor="password" className="block text-left text-gray-600">
+                            Password
+                        </label>
+                        <div className="relative">
+                            <input
+                                type={showPassword ? 'text' : 'password'}
+                                id="password"
+                                {...register("password", {
+                                    required: "* Password is required",
+                                    minLength: {
+                                        value: 8,
+                                        message: "* Password must be at least 8 characters",
+                                    },
+                                    pattern: {
+                                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
+                                        message: "* Password must be alphanumeric and include at least one uppercase letter, one lowercase letter, one number, and one special character",
+                                    },
+                                })}
+                                className={`w-full px-4 py-2 border rounded-md focus:outline-none ${errors.password ? "border-red-500" : "border-gray-300"}`}
+                                placeholder="Enter your password"
+                            />
+                            <button
+                                type="button"
+                                onClick={togglePasswordVisibility}
+                                className="absolute right-2 top-3 text-gray-600 hover:text-gray-800"
+                            >
+                                {showPassword ? (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-5 h-5"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88"
+                                        />
+                                    </svg>
+                                ) : (
+                                    <svg
+                                        xmlns="http://www.w3.org/2000/svg"
+                                        fill="none"
+                                        viewBox="0 0 24 24"
+                                        strokeWidth="1.5"
+                                        stroke="currentColor"
+                                        className="w-5 h-5"
+                                    >
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M2.036 12.322a1.012 1.012 0 0 1 0-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178Z"
+                                        />
+                                        <path
+                                            strokeLinecap="round"
+                                            strokeLinejoin="round"
+                                            d="M15 12a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z"
+                                        />
+                                    </svg>
+                                )}
+                            </button>
+                        </div>
+                        {errors?.password && (
+                            <p className="text-red-500 text-sm mt-1 text-left">
+                                {errors.password.message}
+                            </p>
+                        )}
+                    </div>
+
+                    <div className="my-4">
+                        <p
+                            className="text-blue-600 hover:underline text-sm cursor-pointer"
+                            onClick={() => setWidget('login')}
+                        >
+                            Already have an account? Login
+                        </p>
+                    </div>
+
+                    {/* Submit Button */}
+                    <Button
+                        type="submit"
+                        variant="contained"
+                        color="primary"
+                        className="w-full"
+                        loading={loading}
+                    >
+                        Sign Up
+                    </Button>
+                </form>
+            </Card>
+        </div>
+    );
+};
+
+
+const ForgotPasswordCard: React.FC<AuthCardProps> = ({ setWidget }) => {
     const { register, handleSubmit, formState: { errors } } = useForm<ForgotPasswordFormInputs>();
 
     const onSubmit: SubmitHandler<ForgotPasswordFormInputs> = (data) => {
@@ -250,7 +442,7 @@ const ForgotPasswordCard: React.FC<AuthCardProps> = ({ setForgetPassword }) => {
                 <div className="mt-4">
                     <p
                         className="text-blue-600 hover:underline text-sm cursor-pointer"
-                        onClick={() => setForgetPassword(false)}
+                        onClick={() => setWidget('login')}
                     >
                         Back to Login
                     </p>
